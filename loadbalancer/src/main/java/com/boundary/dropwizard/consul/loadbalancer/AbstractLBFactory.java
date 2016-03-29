38a29cc8430812a32f259abb7bce10112bf0ec02
Jesse Hodges <jesse_hodges@bmc.com>
2016-03-29 15:12:19 -0500

    add cache shutdown

--- loadbalancer/src/main/java/com/boundary/dropwizard/consul/loadbalancer/AbstractLBFactory.java
+++ loadbalancer/src/main/java/com/boundary/dropwizard/consul/loadbalancer/AbstractLBFactory.java
@@ -83,7 +83,7 @@ public abstract class AbstractLBFactory implements LBFactory {
 
             @Override
             public void stop() throws Exception {
-                // todo add cache.shutdown
+                cache.stop();
             }
         });
 
